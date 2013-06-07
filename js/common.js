765b281339fdd419463a377b8a2085c6ee21002e
Georgiy <goshazvir@gmail.com>
2013-06-07 17:08:42 +0300

    add page

--- js/common.js
+++ js/common.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function(){
+
+      $('.js-masonry').masonry({
+        itemSelector: '.photogallery__item',
+
+        columnWidth: 25
+
+
+      });
+
+
+  var $container = $('.js-masonry');
+
+  $container.imagesLoaded( function(){
+    $container.masonry({
+      itemSelector : '.photogallery__item'
+    });
+  });
 
 });
