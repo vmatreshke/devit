aa97e352b367cac52cf9b903cbc30ed93c86769b
Georgiy <goshazvir@gmail.com>
2013-07-18 13:50:49 +0300

    fix

--- js/common.js
+++ js/common.js
@@ -1,13 +1,8 @@
 $(document).ready(function(){
-      api = $('.slider__bg').cycle();
-      $('.content .slider__list a').click(function (e) {
-        api.cycle($(this).parent().index())
-      });
-
       $('.js-masonry').masonry({
         itemSelector: '.photogallery__item',
 
-        columnWidth: 25
+        columnWidth: 10
 
 
       });
