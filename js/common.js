0a95e51f421ea8dbe2d5f9a280925c936cfbbdf5
Georgiy <goshazvir@gmail.com>
2013-07-22 12:04:10 +0300

    fix 1

--- js/common.js
+++ js/common.js
@@ -1,43 +1,6 @@
 $(document).ready(function(){
-
-      $('.content .slider__list a').click(function (e) {
-        api = $('.slider__bg').cycle();
-        api.cycle($(this).parent().index())
-      });
-
-      // $('.js-masonry').masonry({
-      //   itemSelector: '.photogallery__item',
-
-      //   columnWidth: 25
-
-
-      // });
-
-
-      // var $container = $('.js-masonry');
-
-      // $container.imagesLoaded( function(){
-      //   $container.masonry({
-      //     itemSelector : '.photogallery__item'
-      //   });
-      // });
-
-
-    // var container = document.querySelector('#container');
-    // var msnry = new Masonry( container, {
-    //   // options
-    //   columnWidth: 200,
-    //   itemSelector: '.photogallery__item'
-    // });
-
-
-    // var $container = $('#container');
-    // // initialize
-    // $container.masonry({
-    //   columnWidth: 200,
-    //   itemSelector: '.photogallery__item'
-    // });
-
-    // var msnry = $container.data('masonry');
-
+  $('.content .slider__list a').click(function (e) {
+    api = $('.slider__bg').cycle();
+    api.cycle($(this).parent().index())
+  });
 });
