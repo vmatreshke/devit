e7886425b418a08b0d3df2161569c3a2a80bdab2
Georgiy <goshazvir@gmail.com>
2013-07-25 17:19:28 +0300

    pages mobile

--- js/common.js
+++ js/common.js
@@ -1,6 +1,31 @@
 $(document).ready(function(){
-  $('.content .slider__list a').click(function (e) {
-    api = $('.slider__bg').cycle();
-    api.cycle($(this).parent().index())
-  });
+	$('.content .slider__list a').click(function (e) {
+		api = $('.slider__bg').cycle();
+		api.cycle($(this).parent().index())
+	});
+
+
+	$(".navMobile").click(function () {
+		$(".nav").slideToggle("slow");
+		$(".search").slideToggle("fast");
+		$(".h33").toggleClass("btNone");
+	});
+
+  	// for list
+	var firstLink = $(".kat__nav li").first();
+		firstLink.click(function() {
+			if ($(this).hasClass("js-active")) {
+				$(this).removeClass("js-active");
+				$(".kat__nav li").hide();
+				$(this).show();
+			}
+			else {
+				$(this).addClass("js-active")
+				$(".kat__nav li").show();
+			}
+	});
+	// $('.kat__media').append( $('.kat__descr'));
+
+
+
 });
