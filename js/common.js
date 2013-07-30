9bf0705049ec2b711163d2ef33ad2b98bddb461c
Georgiy <goshazvir@gmail.com>
2013-07-30 18:13:14 +0300

    full

--- js/common.js
+++ js/common.js
@@ -14,14 +14,17 @@ $(document).ready(function(){
   	// for list
 	var firstLink = $(".kat__nav li").first();
 		firstLink.click(function() {
-			if ($(this).hasClass("js-active")) {
-				$(this).removeClass("js-active");
-				$(".kat__nav li").hide();
-				$(this).show();
-			}
-			else {
-				$(this).addClass("js-active")
-				$(".kat__nav li").show();
+			var width = $(window).width();
+			if (width > 320 && width < 767){
+				if ($(this).hasClass("js-active")) {
+					$(this).removeClass("js-active");
+					$(".kat__nav li").hide();
+					$(this).show();
+				}
+				else {
+					$(this).addClass("js-active")
+					$(".kat__nav li").show();
+				}
 			}
 	});
 	// $('.kat__media').append( $('.kat__descr'));
@@ -29,15 +32,19 @@ $(document).ready(function(){
 	  	// for list
 		var firstLink = $(".slider__list li").first();
 			firstLink.click(function() {
-				if ($(this).hasClass("js-active")) {
-					$(this).removeClass("js-active");
-					$(".slider__list li").hide();
-					$(this).show();
-				}
-				else {
-					$(this).addClass("js-active")
-					$(".slider__list li").show();
-				}
+				var width = $(window).width();
+				if (width > 320 && width < 767) {
+					if ($(this).hasClass("js-active")) {
+						$(this).removeClass("js-active");
+						$(".slider__list li").hide();
+						$(this).show();
+					}
+					else {
+						$(this).addClass("js-active")
+						$(".slider__list li").show();
+					}
+				};
+				
 		});
 
 });
